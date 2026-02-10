2fd1e0f231fd58586f3c0d4d3729b8d16861ce8b
67070194 <67070194@kmitl.ac.th>
2026-02-11 03:37:20 +0700

    submit exercises

---
+++ cell01/ex00/name.py
@@ -0,0 +1,5 @@
+def main():
+    first_name = "Athikarn"
+    last_name = "Chinjirattikan"
+    print(first_name,last_name)
+main()
---
+++ cell01/ex01/name.py
@@ -0,0 +1,6 @@
+def main():
+    first_name = "Athikarn"
+    last_name = "Chinjirattikan"
+    whole_name = first_name + " " + last_name
+    print(whole_name)
+main()
